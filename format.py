c35938a08c0638995ab21ad93a3028f18f18c9e1
Juan <108938779+OwlWorksInnovations@users.noreply.github.com>
2025-09-25 00:05:04 +0200

    Worked on formatting

--- format.py
+++ format.py
@@ -1,8 +1,48 @@
 import json
+import re
+
 
 def format_posts():
     with open('posts.json', 'r', encoding='utf-8') as f:
         posts = json.load(f)
     
     for post in posts:
-        content = post["submission_post"]
+        content: str = post['submission_post']
+
+        cleaned = re.sub(r"\n", "", content)
+        cleaned = cleaned.replace('(', '').replace(')', '')
+        matches = re.findall(r'\d+[fm]', cleaned, re.IGNORECASE)
+        for match in matches:
+            if match[-1].lower() == 'f':
+                # :-1 keeps the orginal except the last one
+                replacement = match[:-1] + ' female'
+            else:
+                replacement = match[:-1] + ' male'
+
+            cleaned = cleaned.replace(match, replacement)
+            
+        matches2 = re.findall(r"(?<![\'\",.;:()\[\]-])\b[fm]\b", cleaned, re.IGNORECASE)
+        for match2 in matches2:
+            if match2[-1].lower() == 'f':
+                # :-1 keeps the orginal except the last one
+                replacement2 = match2[:-1] + ' female'
+            else:
+                replacement2 = match2[:-1] + ' male'
+
+            cleaned = cleaned.replace(match2, replacement2)
+        
+        abbr_match = re.findall(r'\bAITA\b', cleaned)
+        
+        for abbr_matched in abbr_match:
+            if abbr_matched:
+                abbr_replacement = 'Am I the asshole'
+            else:
+                continue
+            
+            cleaned = cleaned.replace(abbr_matched, abbr_replacement)
+        post["tts_content"] = cleaned
+
+    with open("posts.json", "w", encoding="utf-8") as f:
+        json.dump(posts, f, ensure_ascii=False, indent=3)
+        
+        
