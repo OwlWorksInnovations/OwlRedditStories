5920b571ff9a7f628d94d84fb35163c0968b2f47
Juan <108938779+OwlWorksInnovations@users.noreply.github.com>
2025-09-25 22:27:49 +0200

    Not needed

--- tts.py
+++ tts.py
@@ -8,14 +8,4 @@ async def create_tts(title, text):
     communicate = edge_tts.Communicate(text, voice="en-US-JennyNeural", rate="+5%")
     print(f"[+] Starting TTS for: {title}")
     await communicate.save(f"tts/{title}.mp3")
-
-    with open('posts.json', 'r', encoding='utf-8') as f:
-        posts = json.load(f)
-
-    for post in posts:
-        post["tts"] = 'true'
-
-    with open("posts.json", "w", encoding="utf-8") as f:
-        json.dump(posts, f, ensure_ascii=False, indent=3)
-
     print(f"[+] TTS done for: {title}")
